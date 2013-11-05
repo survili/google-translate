b80847ae1fedb4829c16b6aae02f58d375d99217
survili <tsumbush@gmail.com>
2013-11-05 19:36:12 +0200

    Google seem to be blocking when agent is not sent
    when trying to translate from :auto to :en :text(игнорировать, это тест 1234567890 XXX), I get:
    
    "[[["Ð¸Ð³Ð½Ð¾ÑÐ¸ÑÐ¾Ð²Ð°ÑÑ, Ñ ÑÐ¾ ÑÐµÑ Ñ 1234567890 XXX\r\n","Ð¸Ð³Ð½Ð¾ÑÐ¸ÑÐ¾Ð²Ð°ÑÑ, Ñ ÑÐ¾ ÑÐµÑ Ñ 1234567890 XXX","",""]],,"wo","We are not yet able to translate from Wolof into English.",,,,,[["wo"]],3]"

--- lib/google_translate/google_translate.rb
+++ lib/google_translate/google_translate.rb
@@ -25,7 +25,9 @@ class GoogleTranslate
     begin
       url = GOOGLE_TRANSLATE_SERVICE_URL + "/translate_a/t?client=t&text=#{from_text}&hl=#{from}&sl=#{from}&tl=#{to}&multires=1&prev=btn&ssel=0&tsel=4&uptl=#{to}&alttl=#{from}&sc=1"
 
-      open(URI.escape(url)) do |stream|
+      open(URI.escape(url),
+          "User-Agent" => "Mozilla/5.0 (Windows NT 5.1; rv:10.0.2) Gecko/20100101 Firefox/10.0.2") 
+      do |stream|
         content = stream.read
 
         s = content.split(',').collect { |s| s == '' ? "\"\"" : s }.join(",")
